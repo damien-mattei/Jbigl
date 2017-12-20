e51dae571c2839e0b0fe5bee60a8d053010b37d5
damien.mattei@gmail.com <damien.mattei@unice.fr>
2017-12-20 08:57:07 +0100

    added macro file

---
+++ macro.scm
@@ -0,0 +1,13 @@
+;; macro definition file
+
+(define-syntax display-wasnull 
+  (syntax-rules ()
+    ((_ rs)  (begin
+	       (debug-display "BiglooCode.scm :: ResultatMesuresF : = (java.sql.ResultSet-wasNull rs):")
+	       (debug-display (java.sql.ResultSet-wasNull rs))
+	       (debug-newline)))))
+
+;; (define-syntax error 
+;;   (syntax-rules ()
+;;     ((_ arg1 arg2 arg3 ...)
+;;      (error arg1 arg2 arg3))))
